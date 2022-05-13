d506b74709d17a81215194d850657f1f047e7b84
Yonghai <dylanyonghaih@gmail.com>
2022-05-13 17:01:39 -0400

    finish basic javascript, get the skeleton of the MVP working

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-const wordListEasy = ["apple", "banana","orange","blue", "green"];
+// Spaceman
+// getting a random word from the word list
+const wordListEasy = ["apple", "banana", "orange", "blue", "green"];
+const wordDisplay = document.querySelector("#word");
+let wordChoice = "";
+let wordChoiceArr;
+let newArray = []
+// add event listener to the start button
+const startButton = document.querySelector("#start-button")
+startButton.addEventListener("click", () => {
+    console.log("start!");
+    wordChoice = wordListEasy[Math.floor(Math.random()*wordListEasy.length)];
+    console.log(`the word is ${wordChoice}`);
+    wordChoiceArr = wordChoice.split("");
+    newArray = [];
+    wordChoiceArr.forEach(() => {
+        newArray.push("_")
+    });
+    wordDisplay.innerText = newArray;
+    console.log(newArray);
+
+    
+    
+});
+
+
+
+// getting input from user
+const input = document.querySelector("#input-field");
+const checkMatch = () => {
+    console.log(input.value);
+    for (i = 0; i < newArray.length; i++) {
+        if (input.value == wordChoiceArr[i]) {
+            newArray[i] = input.value; 
+            console.log(newArray);
+        }
+    };
+    
+}
+
+//add event listener to the fire button
+const fireButton = document.querySelector("#fire-button");
+fireButton.addEventListener("click", () => {
+    checkMatch();
+    wordDisplay.innerText = newArray
+})
+
+
+console.log(wordDisplay.innerHTML);
